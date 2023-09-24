cf74f1d6945143ca3636061cd7413b2c7a4dc429
Marat Radchenko <marat@slonopotamus.org>
2023-09-25 00:09:33 +0300

    Fix repo name

--- Formula/macoscontainers.rb
+++ Formula/macoscontainers.rb
@@ -2,9 +2,9 @@ class MacosContainers < Formula
   version "0.0.1"
   depends_on "docker"
   depends_on "docker-buildx"
-  depends_on "macOScontainers/homebrew/bindfs"
-  depends_on "macOScontainers/homebrew/containerd"
-  depends_on "macOScontainers/homebrew/rund"
+  depends_on "macOScontainers/formula/bindfs"
+  depends_on "macOScontainers/formula/containerd"
+  depends_on "macOScontainers/formula/rund"
 
   # Not actually needed for Docker
   # depends_on "macOScontainers/homebrew/buildkitd"
