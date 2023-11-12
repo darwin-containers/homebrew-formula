81e5de8168f38ada0e38b818c20843e78502244d
Marat Radchenko <marat@slonopotamus.org>
2023-11-12 19:10:45 +0300

    Update rund to 0.0.6

--- Formula/rund.rb
+++ Formula/rund.rb
@@ -1,7 +1,7 @@
 class Rund < Formula
-  version "0.0.5"
+  version "0.0.6"
   url "https://github.com/macOScontainers/rund/archive/refs/tags/#{version}.zip"
-  sha256 "1a60cc12eb56feae36a3d68fe9a6fa4b877b4577e68b93e88ba0ebfb643ddd4a"
+  sha256 "0290ddde1b91cbdaa13146156c885d0575925c05a4585247184fd192d8df737f"
 
   depends_on "go" => :build
 
