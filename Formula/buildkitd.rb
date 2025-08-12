521aa5efb226513772bb1687619b429ddc6497ed
Marat Radchenko <marat@slonopotamus.org>
2025-08-12 16:58:57 +0300

    Upgrade buildkit to 0.0.12

--- Formula/buildkitd.rb
+++ Formula/buildkitd.rb
@@ -1,7 +1,7 @@
 class Buildkitd < Formula
-  version "0.0.7"
+  version "0.0.12"
   url "https://github.com/darwin-containers/buildkit/archive/refs/tags/#{version}.zip"
-  sha256 "daaa0668c56cf23141cbc431e38a229615d4b9fb739fd90cbce152aef7c95340"
+  sha256 "834e0c9cebe6147061883dbe53e217465a6ac04efe96606d492ab8656d1c2fb6"
 
   depends_on "go" => :build
 
